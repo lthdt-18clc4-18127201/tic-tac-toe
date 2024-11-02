378fd0a1a62c600c9ee68422f53e5aacb3dfd506
lthdt-18clc4-18127201 <phamtansu2000@gmail.com>
2024-11-02 15:10:07 +0700

    setup client

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className="h-screen w-full flex justify-center items-center flex-col gap-4">
+      <h1  className="text-5xl text-red-800 font-display">Welcome to my App</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <button className="border border-solid border-black px-3">Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
